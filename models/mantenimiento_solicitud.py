dcdcf0314445ad3fd6257b34ddbcd93fda71ba1f
joseCastroB <josecastrobarreto10@gmail.com>
2025-11-19 12:37:24 -0500

    Se agregó el campo taller en solicitud de mantenimiento

--- models/mantenimiento_solicitud.py
+++ models/mantenimiento_solicitud.py
@@ -1,6 +1,11 @@
 
 from odoo import models, fields
 
+class MantenimientoTaller(models.Model):
+    _name = 'mantenimiento.taller'
+    _description = 'Taller de Mantenimiento'
+    name = fields.Char(string='Nombre del Taller', required=True)
+
 class MaintenanceRequest(models.Model):
     
     _inherit = 'maintenance.request'
@@ -8,9 +13,17 @@ class MaintenanceRequest(models.Model):
     tecnico_id = fields.Many2one(
         'res.users',
         string='Técnico',
+        tracking=True,
     )
     
     planner_id = fields.Many2one(
         'res.users',
         string='Planner',
+        tracking=True,
+    )
+
+    taller_id = fields.Many2one(
+        'mantenimiento.taller',
+        string='Taller',
+        
     )
